b00752262ad71dccf5e7f9b52c77b31f7b66a2fd
dexterg <pascal.gautherot@gmail.com>
2020-03-14 18:31:47 +0100

    add routes api/v1 and api/v2 frame

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Api definition
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      # We are going to list our resources here
+    end
+    namespace :v2 do
+      # We are going to list our resources here
+    end
+  end
 end
